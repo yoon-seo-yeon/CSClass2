c48de27c3a34f6cd1042e7a1f8c6a9837dacd55f
yoon-seo-yeon <s2013@e-mirim.hs.kr>
2022-08-25 12:11:30 +0900

    #4 구조체 실습

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSClass2
 {
     internal class Program
+
     {
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+
         static void Main(string[] args)
         {
             Wanted<string> wantedString = new Wanted<string>("String");
@@ -43,12 +51,70 @@ namespace CSClass2
             NextPos(x, y, vx, vy, out x, out y);
             Console.WriteLine("다음 좌표 : (" + x + ", " + y + ")");
 
+            Point point;
+            point.x = 10;
+            point.y = 10;
+            point = new Point(100, 200);
+            point = new Point();
+            Console.WriteLine(point.x + " / " + point.y);
+
+            PointClass pcA = new PointClass(10, 20);
+            PointClass pcB = pcA;
+            pcB.x = 100; pcB.y = 200;
+            Console.WriteLine(pcA.x + " / " + pcA.y);
+            Console.WriteLine(pcB.x + " / " + pcB.y);
+
+            PointStruct psA = new PointStruct(10, 20);
+            PointStruct psB = psA;
+            psB.x = 100; psB.y = 200;
+            Console.WriteLine(psA.x + " / " + psA.y);
+            Console.WriteLine(psB.x + " / " + psB.y);
         }
 
-        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        class PointClass
         {
-            rx = x + vx;
-            ry = y + vy;
+            public int x;
+            public int y;
+            public PointClass(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        struct PointStruct
+        {
+            public int x;
+            public int y;
+
+            public PointStruct(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }   
+
+        }
+
+        struct Point
+        {
+            public int x;
+            public int y;
+            public string testA;
+            public string testB;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = "초기화";
+                this.testB = "초기화";
+            }
+
+            public Point(int x, int y, string s)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = s;
+                this.testB = s;
+            }
         }
     }
 }
