908de1badf0c2c3053127006faa6dddb9a5cdcbb
yoon-seo-yeon <s2013@e-mirim.hs.kr>
2022-08-30 15:34:25 +0900

    #6 인터페이스 소개 : IComparable, IDiposable 실습

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -117,18 +117,29 @@ namespace CSClass2
             Console.WriteLine(psA.x + " / " + psA.y);
             Console.WriteLine(psB.x + " / " + psB.y);
 
-            List<Product> list = new List<Product>()
+            using(Dummy dummy = new Dummy())
             {
-                new Product(){Name="고구마", Price=1500},
-                new Product(){Name="사과", Price=2400},
-                new Product(){Name="바나나", Price=1000},
-                new Product(){Name="배", Price=3000},
-            };
-            list.Sort();
+                List<Product> list = new List<Product>()
+                {
+                    new Product(){Name="고구마", Price=1500},
+                    new Product(){Name="사과", Price=2400},
+                    new Product(){Name="바나나", Price=1000},
+                    new Product(){Name="배", Price=3000},
+                };
+                list.Sort();
 
-            foreach(var item in list)
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }  
+        }
+
+        class Dummy : IDisposable
+        { 
+            public void Dispose()
             {
-                Console.WriteLine(item);
+                Console.WriteLine("Dispose() 메서드를 호출했습니다.");
             }
         }
     }
